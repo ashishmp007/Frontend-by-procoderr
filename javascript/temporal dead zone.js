568e9b62964ee83f0132bbfd1d8875c295b4132a
ashishmp007 <ashishphadtare007@gmail.com>
2026-03-08 10:00:00 +0530

    Javascript day 3

---
+++ javascript/temporal dead zone.js
@@ -0,0 +1,14 @@
+console.log(a);
+
+var a = "ashish";
+console.log(a);
+
+// console.log(b)
+
+let b = "phadtare";
+console.log(b);
+
+// console.log(c)
+
+const c = "ashish phadtare";
+console.log(c);
---
+++ javascript/variables.js
@@ -0,0 +1,9 @@
+let firstname = "Ashish";
+let lastname = "Phadtare";
+let age = 27;
+let isMarried = false;
+let a;
+let userIntro =
+  "Hi, my name is " + firstname + " " + lastname + ". I am " + age + " years old";
+
+  
