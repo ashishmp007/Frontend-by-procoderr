998ee60d98b497ca03005f1995cb7d43f593cbc9
ashishmp007 <ashishphadtare007@gmail.com>
2026-03-09 10:00:00 +0530

    Javascript day 4

---
+++ javascript/Dialogue boxes.js
@@ -0,0 +1,9 @@
+// alert("Hello, World!");
+
+if (confirm("Are you sure?")) {
+    // User clicked "OK"
+} else {
+    // User clicked "Cancel"
+}
+
+var name = prompt("What is your name?","Please enter your name here");
